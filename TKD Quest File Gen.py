46820363bd90188a6f0ae165eb2049a9f0749ed8
Nataku62 <nataku6262@hotmail.co.uk>
2016-07-27 22:17:40 +0100

    Addition of python Files to generate pickled lists as question library, Quiz application and 3 question libraries in text files.

---
+++ TKD Quest File Gen.py
@@ -0,0 +1,38 @@
+import pickle
+import os
+
+fileList = ['wbQuestion.txt','ytQuestion.txt', 'ybQuestion.txt']
+
+def questList(fileName):
+    quiz = []
+    for file in os.listdir ('\\Quiz Questions'):
+        if file.startswith (fileName):
+            quiz.append(file)
+    return quiz
+       
+def whiteSpace(listNam, val):
+    return [value for value in listNam if value != val]
+
+def vinegar():
+    question = []
+    
+    for file in fileList:
+        n=1
+        quizFile = open ('\\Question Files\\' + file, 'r')
+        for line in quizFile.readlines():
+            for i in line.split('\n'):
+                i.rstrip('\n')
+                question.append(i)
+                
+
+        question = whiteSpace(question, '')
+        
+        while len(question) >0:
+            tempList = question[0:7]
+            questFile = open('\\Quiz Questions\\'+ file +str(n)+'.p', 'wb')
+            pickle.dump(tempList, questFile)
+            questFile.close()
+            del question[0:7]
+            n+=1
+
+vinegar()
